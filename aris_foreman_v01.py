f216395568ba37ee600cc70b6dd209b39d18c58d
Aslan-Khashtyrov <aslan.khashtyrov06@gmail.com>
2026-08-26 10:56:05 +0300

    Use Termux control as sole Git report writer

--- aris_foreman_v01.py
+++ aris_foreman_v01.py
@@ -25,7 +25,6 @@ COMPONENTS = {
     "guardian": "guardian_v04.py",
     "worker": "aris_worker_v03.py",
     "autopilot": "aris_autopilot_v01.py",
-    "remote_agent": "aris_remote_agent_v02.py",
     "termux_control": "aris_termux_control_v01.py",
 }
 
